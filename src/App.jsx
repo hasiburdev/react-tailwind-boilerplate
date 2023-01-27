8a5fd80a541045ad16482b059e43f253b6ef9381
Hasibur Rahman <hasiburdev@gmail.com>
2023-01-27 11:57:42 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import RootLayout from "./components/layout/RootLayout";
+import Home from "./pages/Home";
+
+let router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+    </Route>
+  )
+);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div className="px-4 py-10 text-xl text-blue-400">Home Page</div>;
+};
+
+export default Home;
